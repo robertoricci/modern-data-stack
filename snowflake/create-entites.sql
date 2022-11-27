d70596318a7394d6913123d54e52b8c116ef3680
Roberto Carlos Ricci <roberto.rricci@gmail.com>
2022-11-27 18:27:15 +0000

    snowflake inti

---
+++ snowflake/create-entites.sql
@@ -0,0 +1,67 @@
+set airbyte_role = 'SEU_ROLE';
+ set airbyte_username = 'SEU_USER';
+ set airbyte_warehouse = 'SEU_WAREHOUSE';
+ set airbyte_database = 'SEU_DATABASE';
+ set airbyte_schema = 'SEU_SCHEMA';
+
+ -- set user password
+ set airbyte_password = 'SEU_PASSWORD';
+
+ begin;
+
+ -- create Airbyte role
+ use role securityadmin;
+ create role if not exists identifier($airbyte_role);
+ grant role identifier($airbyte_role) to role SYSADMIN;
+
+ -- create Airbyte user
+ create user if not exists identifier($airbyte_username)
+ password = $airbyte_password
+ default_role = $airbyte_role
+ default_warehouse = $airbyte_warehouse;
+
+ grant role identifier($airbyte_role) to user identifier($airbyte_username);
+
+ -- change role to sysadmin for warehouse / database steps
+ use role sysadmin;
+
+ -- create Airbyte warehouse
+ create warehouse if not exists identifier($airbyte_warehouse)
+ warehouse_size = xsmall
+ warehouse_type = standard
+ auto_suspend = 60
+ auto_resume = true
+ initially_suspended = true;
+
+ -- create Airbyte database
+ create database if not exists identifier($airbyte_database);
+
+ -- grant Airbyte warehouse access
+ grant USAGE
+ on warehouse identifier($airbyte_warehouse)
+ to role identifier($airbyte_role);
+
+ -- grant Airbyte database access
+ grant OWNERSHIP
+ on database identifier($airbyte_database)
+ to role identifier($airbyte_role);
+
+ commit;
+
+ begin;
+
+ USE DATABASE identifier($airbyte_database);
+
+ -- create schema for Airbyte data
+ CREATE SCHEMA IF NOT EXISTS identifier($airbyte_schema);
+
+ commit;
+
+ begin;
+
+ -- grant Airbyte schema access
+ grant OWNERSHIP
+ on schema identifier($airbyte_schema)
+ to role identifier($airbyte_role);
+
+ commit;
